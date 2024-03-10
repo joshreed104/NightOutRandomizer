734a3fd49d7b965a487ce3e454f85f38574a9513
Josh Reed <joshreed104@gmail.com>
2024-03-09 23:16:59 -0800

    Adds fetching functions to dev server api

---
+++ placeNetwork/placesNetwork.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { DiaryEntry } from '../PlaceItem/PlaceItem';
+
+const baseUrl = 'http://localhost:8000';
+
+export const addPlaceToDiary = async ({
+  name,
+  rating,
+  neighborhood,
+  placeUrl,
+  photoUrl,
+  user_rating,
+}: DiaryEntry): Promise<void> => {
+  try {
+    const response = await axios.post(`${baseUrl}/places`, {
+      name,
+      rating,
+      neighborhood,
+      place_url: placeUrl,
+      photo_url: photoUrl,
+    });
+    return response.data;
+  } catch (error) {
+    console.log('Error saving Place: ', error);
+  }
+};
+
+export const fetchSavedPlaces = async () => {
+  try {
+    const response = await axios.get(`${baseUrl}/places`);
+    return response.data;
+  } catch (error) {
+    console.log('Error fetching places: ', error);
+  }
+};
